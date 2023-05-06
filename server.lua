fe6334333f104b13f61d9667169f851467ad7edb
Ross <rosshamey@gmail.com>
2023-05-05 20:38:15 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -6,7 +6,12 @@ local false_origin = gpr:WaitForChild("Handle")
 local fire_remote = gpr:WaitForChild("OnShoot")
 
 local Workspace = game:GetService("Workspace")
+local ServerStorage = game:GetService("ServerStorage")
 
 fire_remote.OnServerEvent:Connect(function(player, position)
-	-- later
+	local origin = true_origin.Position
+	local direction = (position - origin).Unit
+	local result = Workspace:Raycast(true_origin.Position, direction*300)
+	
+	local intersection = result and result.Position or true_origin + direction*300
 end)
